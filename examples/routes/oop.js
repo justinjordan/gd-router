bca7f4c2b4d3abc65e1327698f877e8a34fe2c58
Aaron <aaron4uk@gmail.com>
2019-06-01 22:15:17 -0500

    chore: updating my comments

--- examples/routes/oop.js
+++ examples/routes/oop.js
@@ -4,8 +4,8 @@ class oop extends Controller
 {
     /**
      * Handles GET requests
-     * @param {Hapi/Request} request
-     * @return {string}
+     * @param {request} request
+     * @return {string|object}
      */
     get(request) {
         return "<h1>Hello!</h1><p>I am a url route created by an object oriented javascript file, weeee!</p>"
